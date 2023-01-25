00633f604bf421b0bde811b3c95ec15425804e90
Artur <sultanovartur1873@gmail.com>
2023-01-25 22:34:51 +0300

    [fix]: grpc

--- internal/handlers/grpc/grpcHandlers.go
+++ internal/handlers/grpc/grpcHandlers.go
@@ -1,10 +1,12 @@
 package grpcHandlers
 
 import (
+	pb "burmachine/LinkGenerator/gen/go/protos"
 	"golang.org/x/mod/sumdb/storage"
 )
 
 type GrpcHandlers struct {
-	//server  *server2.Server
+	pb.UnimplementedServiceNameServer
+	// server  *server2.Server
 	storage *storage.Storage
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,22 +1,25 @@
 package server
 
 import (
-	"burmachine/LinkGenerator/internal/config"
-	httpHandlers "burmachine/LinkGenerator/internal/handlers/http"
 	"context"
 	"flag"
-	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
+	"log"
+	"net"
 	"net/http"
 
 	gw "burmachine/LinkGenerator/gen/go/protos"
+	"burmachine/LinkGenerator/internal/config"
+	grpcHandlers "burmachine/LinkGenerator/internal/handlers/grpc"
+	httpHandlers "burmachine/LinkGenerator/internal/handlers/http"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"google.golang.org/grpc"
 )
 
 type Server struct {
 	httpHandles *httpHandlers.HttpHandlers
 	conf        config.Conf
 	Mux         *runtime.ServeMux
+	grpcHadles  grpcHandlers.GrpcHandlers
 }
 
 func NewServerWithConfiguration(conf config.Conf) *Server {
@@ -24,17 +27,25 @@ func NewServerWithConfiguration(conf config.Conf) *Server {
 }
 
 func (s *Server) Run(ctx context.Context) error {
-	//ctx := context.Background()
-	//ctx, cancel := context.WithCancel(ctx)
-	//defer cancel()
+	// ctx := context.Background()
+	// ctx, cancel := context.WithCancel(ctx)
+	// defer cancel()
 
 	grpcServerEndpoint := flag.String("grpc-server-endpoint", s.conf.AddrGrpc, "gRPC server endpoint")
 
-	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
-	err := gw.RegisterServiceNameHandlerFromEndpoint(ctx, s.Mux, *grpcServerEndpoint, opts)
+	// opts := []grpc.ServerOption{insecure.()}
+	// err := gw.RegisterServiceNameHandlerFromEndpoint(ctx, s.Mux, *grpcServerEndpoint, opts)
+	// if err != nil {
+	// 	return err
+	// }
+
+	lis, err := net.Listen("tcp", *grpcServerEndpoint)
 	if err != nil {
-		return err
+		log.Fatalf("failed to listen: %v", err)
 	}
+	grpcServer := grpc.NewServer()
+	gw.RegisterServiceNameServer(grpcServer, &s.grpcHadles)
+	grpcServer.Serve(lis)
 
 	// Start HTTP server (and proxy calls to gRPC server endpoint)
 	return http.ListenAndServe(s.conf.AddrHttp, s.Mux)
